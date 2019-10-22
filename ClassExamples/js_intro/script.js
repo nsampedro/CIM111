cda3304f8a90ddbd9c9543846e1b2e1df3e9c818
Nicolas Sampedro <nxs310@miami.edu>
2019-10-21 22:26:27 -0400

    javascript intro

---
+++ ClassExamples/js_intro/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // all your javascript goes here
+    //how to add numbers
+    var x = 2+2;
+    //be careful where you put you quotations:
+    var y = 2 + "2";
+    //defining a variable
+    console.log(x);
+    console.log("Everything loaded!");
+    console.log("this function ran");
+});
+
+
+// var array1 = [1,2,3,4,5,6];
+// var array2 = ["This is a string", "this is also a string"];
+// var heading = document.getElementsByTagName("h1");
+// var x = 4;
+// console.log(heading);
+// console.log(x);
+// console.log(document.getElementsByTagName("h1"));
