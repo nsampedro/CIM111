d64f7f7a77a02bcb43cd959098f96ab68e66ce55
Nicolas Sampedro <nxs310@miami.edu>
2019-11-04 12:35:59 -0500

    update

--- HW/jQuery_project/script.js
+++ HW/jQuery_project/script.js
@@ -2,17 +2,19 @@ var ldnClicked = false;
 
 
 $(document).ready(function() {
-   $("#visit").click(function() {
-    if (ldnClicked === false) {
+ $("#visit").click(function() {
+  console.log("test");
+  if (ldnClicked === false) {
 
-     $("#visit").css("font-size", "60px");
-     ldnClicked = true;
-    } else {
+   $("#visit").css("font-size", "80px");
+   ldnClicked = true;
+  } else {
 
-     $("#visit").css("font-size", "40px");
-     ldnClicked = false;
+   $("#visit").css("font-size", "60px");
+   ldnClicked = false;
 
 
-    }
+  }
 
-   });
+ });
+});
