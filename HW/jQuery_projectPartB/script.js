99c23d1dbddd7fd8acfc83059220a563f2ad1318
Nicolas Sampedro <nxs310@miami.edu>
2019-11-06 13:32:18 -0500

    adding plugins to hw

---
+++ HW/jQuery_projectPartB/script.js
@@ -0,0 +1,78 @@
+var ldnClicked = false;
+var color = 0;
+
+$(document).ready(function() {
+ $("#visit").click(function() {
+  // console.log("test");
+  if (ldnClicked === false) {
+
+   $("#visit").css("font-size", "80px");
+   ldnClicked = true;
+  } else {
+
+   $("#visit").css("font-size", "60px");
+   ldnClicked = false;
+
+
+  }
+ });
+
+ $("#learn").click(function() {
+  if (color === 0) {
+   // color is 0. switch to the first color
+   $("#learn").css("color", "red");
+   color = 1;
+  } else if (color === 1) {
+   // color is 1
+   $("#learn").css("color", "blue");
+   color = 2;
+  } else if (color === 2) {
+   // color is 2
+   $("#learn").css("color", "white");
+   color = 3;
+  } else {
+   $("#learn").css("color", "black");
+   color = 0;
+  }
+ });
+
+ $("#bye").click(function() {
+  $("#byeMessage").html("Hey! Have a good trip!")
+ });
+
+ $("#hide").click(function() {
+  $("#goAway").hide(1000);
+ });
+
+});
+
+// $(document).ready(function() {
+//  $("#learn").toggle(
+//   function() {
+//    $("#learn").css(
+//     "color",
+//     "red"
+//    );
+//   },
+//   function() {
+//    $("#learn").css(
+//     "color",
+//     "blue"
+//    );
+//   },
+//   function() {
+//    $("#learn").css(
+//     "color",
+//     "white"
+//    );
+//   });
+// });
+
+// $(document).ready(function() {
+//  $("#goodTrip").click(function() {
+//   $("p").hide();
+//  });
+//  $("#goodTrip").click(function() {
+//   $("p").show();
+//  });
+// });
