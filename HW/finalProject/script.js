ec431359061551918f46c63b1d725cd2016f3952
Nicolas Sampedro <nxs310@miami.edu>
2019-12-08 19:59:01 -0500

    mobile final
    working on mobile css for final

--- HW/finalProject/script.js
+++ HW/finalProject/script.js
@@ -1,3 +1,20 @@
+var titleClicked = false;
+
+$("#visit").click(function() {
+ // console.log("test");
+ if (titleClicked === false) {
+
+  $("#visit").css("font-size", "80px");
+  titleClicked = true;
+ } else {
+
+  $("#visit").css("font-size", "60px");
+  titleClicked = false;
+
+
+ }
+});
+
 $("#bye").click(function() {
  $("#byeMessage").html("Hey! Have a good trip! :-)")
 });
